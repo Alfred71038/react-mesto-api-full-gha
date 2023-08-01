54c512c02f4fc1233e6ef1d636e2ebac28c5c8ff
Ageev Linar <ageevlinar@yandex.ru>
2023-08-01 17:21:21 +0300

    Бэк

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,8 @@ const router = require('./routes/index');
 
 const error = require('./middlewares/error');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => {
     console.log('Подключение к mongodb.');
@@ -27,7 +29,11 @@ app.use(bodyParser.json());
 
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
