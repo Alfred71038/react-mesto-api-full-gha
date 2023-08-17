5173fb26e9ed4a34b273c2a56fcec4b7a2d93d3c
Ageev Linar <ageevlinar@yandex.ru>
2023-08-18 00:33:43 +0300

    Корс

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -57,7 +57,7 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret',
         { expiresIn: '7d' },
       );
       return res.cookie('jwt', token, {
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -9,7 +9,7 @@ const auth = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
   } catch (err) {
     return next(new UnauthorizedError('Авторизуйтесь'));
   }
