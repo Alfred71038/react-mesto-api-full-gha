2756afd4ca6bb56ee3a8654f22d67add35cb8684
Ageev Linar <ageevlinar@yandex.ru>
2023-08-19 12:19:04 +0300

    Корс

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -9,9 +9,24 @@ const auth = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    console.log('\x1b[31m%s\x1b[0m', `
+Надо исправить. В продакшне используется тот же
+секретный ключ, что и в режиме разработки.
+`);
   } catch (err) {
-    return next(new UnauthorizedError('Авторизуйтесь'));
+    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+      console.log(
+        '\x1b[32m%s\x1b[0m',
+        'Всё в порядке. Секретные ключи отличаются'
+      );
+    } else {
+      console.log(
+        '\x1b[33m%s\x1b[0m',
+        'Что-то не так',
+        err
+      );
+    }
   }
 
   req.user = payload;
